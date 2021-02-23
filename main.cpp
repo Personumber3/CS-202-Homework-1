482216f048ea597a309b9748cb7c3030e69550d5
Alden Ahern <alden@biofidelic.comgit config --unset user.namegit config --global user.name alden>
2021-02-22 23:29:58 -0900

    added 4 functions and tests each on increasingly large objects

--- main.cpp
+++ main.cpp
@@ -19,33 +19,56 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    
-    Stopwatch watchymcwatchface;
 
+    Stopwatch watchymcwatchface;
+        
     cout << "meowth, that's right" << endl;
     watchymcwatchface.endwatch();
     cout << "time taken " << watchymcwatchface.gettime() << " seconds" << endl;
-    cout << watchymcwatchface.getmilliseconds() << " milliseconds" << endl;
+    cout << "time taken " << watchymcwatchface.getmilliseconds() << " milliseconds" << endl;
 
-        auto seed = chrono::high_resolution_clock();
-    mt19937 timeywimey(seed.now().time_since_epoch().count());
+    // vector<int> nums;
+    auto meow = chrono::high_resolution_clock();
+    mt19937 timeywimey(meow.now().time_since_epoch().count());
     
     vector<int> numberset;
     vector<int> searchset{42};
-    int max = 10;
+    int max;
     int i = 0;
-    for(int j = i; j < max; ++j){
-        numberset.push_back(timeywimey());
-    }
     Stopwatch watchy(watchymcwatchface);
-    search(numberset.begin(),numberset.end(),searchset.begin(),searchset.end());
     watchy.endwatch();
-    cout << max << " items search time " << watchy.gettime() << " seconds." << endl;
-    
     Stopwatch watchface(watchymcwatchface);
+    watchface.endwatch();
+    Stopwatch watchy_watch(watchymcwatchface);
+    watchy_watch.endwatch();
+    for(int k = 1; k < 10; ++k){
+        max = pow(10,k);
+        for(int j = i; j < max; ++j){
+            numberset.push_back(timeywimey());
+        }
+        watchy.restartwatch();
+        search(numberset.begin(),numberset.end(),searchset.begin(),searchset.end());
+        watchy.endwatch();
+        cout << max << " items search time: " << watchy.gettime() << " seconds." << endl;
+        
+        watchface.restartwatch();
+        binary_search(numberset.begin(),numberset.end(),42);
         watchface.endwatch();
+        cout << max << " items binary search: " << watchface.gettime() << " seconds." << endl;
+
+        watchymcwatchface.restartwatch();
+        max_element(numberset.begin(),numberset.end());
+        watchymcwatchface.endwatch();
+        cout << max << " items max element: " << watchymcwatchface.gettime() << " seconds." << endl;
+
+        watchy_watch.restartwatch();
+        shuffle(numberset.begin(),numberset.end(),timeywimey);
+        watchy_watch.endwatch();
+        cout << max << " items shuffle time: " << watchy_watch.gettime() << " seconds." << endl;
+
         
-    cout << "time taken " << watchy.getmilliseconds() << " milliseconds" << endl;
-        
+        i = max;
+    }
+    
         return 0;
 }
